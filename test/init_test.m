39b0a4dc39fd3c7b3c92d5686275806b6fc2f3d0
micheleciciolla <mikele_04@hotmail.it>
2020-09-08 09:57:42 +0200

    update - model

--- test/init_test.m
+++ test/init_test.m
@@ -1,13 +1,14 @@
 clear
+cd /home/mic-ciciolla/Documents/università/incompleted/elective/4_multirobot/vehicles-platooning/test
 %% PARAMETERS
 M = 1000; % mass [kg] of each vehicle
 
 % initial condition states [r, v] - STEADY STATE
-n0_init = [0 0];
+n0_init = [3 0];
 n1_init = [-5 0];
 n2_init = [-10 0];
-n3_init = [-15 0];
-n4_init = [-20 0];
+n3_init = [-20 0];
+n4_init = [-30 0];
 
 % adjacency matrix
 % N = 4
@@ -28,24 +29,37 @@ d4 = 2;
 
 % spacing policy
 h10 = 0.8; % [s]
-h20 = h10*2;
-h30 = h10*3;
-h40 = h10*4;
+h20 = 3*h10;
+h30 = 3*h20;
+h40 = 3*h30;
 
 % stifness and damping coefficient
-k = 0.5; % (?)
-b = 0.5; % (?)
+% maybe we should consider to have one coefficient for each car
+k = 120; % (?)
+b = 270; % (?)
+
+% individual params (to-do)
+b1 = 120;
+k1 = 270;
+
+b2 = 120;
+k2 = 270;
+
+b3 = 120;
+k3 = 270;
+
+b4 = 120;
+k4 = 270;
 
 % --------------------------------------------------------------
 %% SIMULATION
 
 % temporal inputs
 % the leader imposes a constant fleet vel = u0 = 20 [m/s] = 72 [km/h]
-[u0,u1,u2,u3,u4] = deal(20,1,1,1,1);
-
+u0 = 20;
 
 % start simulation
-simtime = 50;
+simtime = 300;
 output = sim('model.slx',simtime);
 
 fprintf(2,"~\nProcess completed!\n~\n");
