e272a820ee94cac6b080abc7c9ed447ddb8b56bb
micheleciciolla <mikele_04@hotmail.it>
2020-09-09 19:26:11 +0200

    hij fixed

--- test/init_test.m
+++ test/init_test.m
@@ -1,14 +1,23 @@
-clear
-cd /home/mic-ciciolla/Documents/università/incompleted/elective/4_multirobot/vehicles-platooning/test
+try
+    cd /home/mic-ciciolla/Documents/università/incompleted/elective/4_multirobot/vehicles-platooning/test
+    disp("directory changed");
+catch
+    return
+
+end
+
+
+    close all
+
 %% PARAMETERS
-M = 1000; % mass [kg] of each vehicle
+M = 1200; % mass [kg] of each vehicle
 
 % initial condition states [r, v] - STEADY STATE
-n0_init = [3 0];
-n1_init = [-5 0];
-n2_init = [-10 0];
-n3_init = [-20 0];
-n4_init = [-30 0];
+n0_init = [0 0];
+n1_init = [-3 0];
+n2_init = [-8 0];
+n3_init = [-13 0];
+n4_init = [-18 0];
 
 % adjacency matrix
 % N = 4
@@ -28,38 +37,72 @@ d3 = 3;
 d4 = 2;
 
 % spacing policy
-h10 = 0.8; % [s]
-h20 = 3*h10;
-h30 = 3*h20;
-h40 = 3*h30;
+h10 = -0.8; % [s]
+h20 = h10*2;
+h30 = h10*3;
+h40 = h10*4;
 
 % stifness and damping coefficient
+% b, kij >0
 % maybe we should consider to have one coefficient for each car
-k = 120; % (?)
-b = 270; % (?)
+b = 550;
 
 % individual params (to-do)
-b1 = 120;
-k1 = 270;
-
-b2 = 120;
-k2 = 270;
-
-b3 = 120;
-k3 = 270;
-
-b4 = 120;
-k4 = 270;
+k1 = 120;
+k2 = 0;
+k3 = 0;
+k4 = 0;
 
 % --------------------------------------------------------------
 %% SIMULATION
 
-% temporal inputs
 % the leader imposes a constant fleet vel = u0 = 20 [m/s] = 72 [km/h]
+% this is a constant VELOCITY input
 u0 = 20;
 
 % start simulation
-simtime = 300;
-output = sim('model.slx',simtime);
+simtime = 200;
+out = sim('model.slx',simtime);
 
 fprintf(2,"~\nProcess completed!\n~\n");
+
+clc
+
+%% Getting data from Simulink - state variable structure
+t = out.r0.time;
+dim = size(t);
+
+r0 = out.r0.signals.values;
+r1 = out.r1;
+r2 = out.r2;
+r3 = out.r3;
+r4 = out.r4;
+
+v0 = out.v0;
+v1 = out.v1;
+v2 = out.v2;
+v3 = out.v3;
+v4 = out.v4;
+
+err10 = out.err10;
+err20 = out.err20;
+err30 = out.err30;
+err40 = out.err40;
+
+figure()
+hold on, grid minor
+plot(t,[err10,err20,err30,err40],'-.');
+legend('r1-r0-h10v0','r2-r0-h20v0','r3-r0-h30v0','r4-r0-h40v0');
+xlabel("time");
+ylabel("error");
+title("car distance error");
+
+disp("Final err1");
+disp(err10(end))
+
+
+
+
+
+
+
