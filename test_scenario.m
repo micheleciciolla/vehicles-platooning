db67707a6059574bedf132be87237db680c23555
micheleciciolla <mikele_04@hotmail.it>
2020-09-11 17:01:58 +0200

    driving scenario sim

---
+++ test_scenario.m
@@ -0,0 +1,4 @@
+% https://it.mathworks.com/help/driving/examples/driving-scenario-tutorial.html
+% ho già creato lo scenario con una strada dritta ed i 4 attori
+% devo vedere come simularli in tempo reale
+[scenario,egoVehicle] = createDrivingScenario();
